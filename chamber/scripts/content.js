371602b7f860d058566e28520bccd9847886404a
0x-moon-x0 <kdruchok@byupathway.edu>
2025-03-17 05:11:55 -0600

    chamber update test

---
+++ chamber/scripts/content.js
@@ -0,0 +1,9 @@
+const url = 'https://0x-moon-x0.github.io/wdd231/chamber/data/members.json';
+
+const cards = document.querySelector('#cards');
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+
+async function getMembers() {
+    
+}
